ed8db63dbcdd6edbd02d7c6c74d4c225b5cf12e5
Minaev Vlad <minaev-vlad@yandex.ru>
2020-04-20 17:05:44 +0300

    Выполнил задания 4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,14 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let row of table.rows) {
+    if (row.cells[3].dataset.available === undefined) {row.setAttribute('hidden', '');}
+    else {
+      (row.cells[3].dataset.available == 'true') ? row.classList.add(`available`) : row.classList.add(`unavailable`);}
+        
+    row.cells[2].innerHTML === 'm' && row.classList.add(`male`);
+    row.cells[2].innerHTML === 'f' && row.classList.add(`female`);
+        
+    if (parseInt(row.cells[1].innerHTML) < 18) {row.style.cssText = "text-decoration: line-through";}
+  }   
 }
