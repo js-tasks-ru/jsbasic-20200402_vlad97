b251b483503a00d3b86fab461437cd4780c0a02e
Minaev Vlad <minaev-vlad@yandex.ru>
2020-04-27 08:33:51 +0300

    Написал решение для задачи 5.2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let buttons = document.getElementsByClassName('toggle-text-button');
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+      let elem = document.getElementById('text');
+      elem.hidden = !elem.hidden;
+    });
+  }
 }
